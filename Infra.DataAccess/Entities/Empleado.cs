bffa50f734947f571b83501a318dee9ea3ca7ac6
Jose Martinez <jlmartinezmarquez@users.noreply.github.com>
2020-01-31 07:53:32 +0100

    Code styling

--- Infra.DataAccess/Entities/Empleado.cs
+++ Infra.DataAccess/Entities/Empleado.cs
@@ -6,6 +6,8 @@ namespace Infra.DataAccess.Entities
 {
     public class Empleado
     {
+        // JLM - Convencion sobre nombres: las propiedades publicas siempre se escriben en Pascal Case. Asi, seria Id en vez de ID
+        // JLM - Es muy probable que tener instalado Style Cop te ayude con estas cosas
         public int ID { get; set; }
         public string Nombre { get; set; }
         public string Apellido { get; set; }
